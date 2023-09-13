2e3a0588b1504ff1cf937a5fc3b51c113f92b729
Tommy Abraham <89310807+binmulindi6@users.noreply.github.com>
2023-09-13 09:15:01 +0200

    update on CoursController

--- app/Http/Controllers/CoursController.php
+++ app/Http/Controllers/CoursController.php
@@ -61,7 +61,7 @@ class CoursController extends Controller
             'max_examen' => ['required','string','max:255'],
         ]);
         
-        $niveau = Classe::findOrFail($request->niveau);
+        $niveau = Niveau::findOrFail($request->niveau);
         $categorie_cours = CategorieCours::find($request->categorie_cours);
 
         //dd($classe, $categorie_cours);
