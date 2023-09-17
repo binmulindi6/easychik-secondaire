48a30a409e2d49bd2511e34a38763790c16d15b7
Tommy Abraham <89310807+binmulindi6@users.noreply.github.com>
2023-09-18 01:10:53 +0200

    review 😂

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -365,10 +365,10 @@
                     <li class="mt-0.5 w-full">
                         @if (str_contains($page_name, 'Presences'))
                             <a class="py-2.7 bg-blue-500/13 dark:text-white dark:opacity-80 text-size-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 font-semibold text-slate-700 transition-colors"
-                                href="{{ route('presences.index') }}">
+                                href="{{ route('presences') }}">
                             @else
                                 <a class=" hover:bg-blue-500/13 rounded-lg dark:text-white dark:opacity-80 py-2.7 text-size-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors"
-                                    href="{{ route('presences.index') }}">
+                                    href="{{ route('presences') }}">
                         @endif
                         <div
                             class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
